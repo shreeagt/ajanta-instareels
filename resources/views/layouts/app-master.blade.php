8cd9d47ccf7a78a5dbf9fff3f3518a1902ff5696
sachhidanandagt <guptasachidananad.agt@gmail.com>
2023-06-12 15:35:38 +0530

    first initialize commit

---
+++ resources/views/layouts/app-master.blade.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.87.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    @stack('title')
+    <!-- Favicons -->
+    <link href="{{ asset('assets/images/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets/images/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- DataTables -->
+    <link href="{{asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css')}}" rel="stylesheet" />
+
+    <!-- Responsive datatable examples -->
+    <link href="{{asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+
+    <link href="{{ asset('assets/libs/chartist/chartist.min.css') }}" rel="stylesheet">
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" />
+    <!-- Icons Css -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" />
+    <!-- App Css-->
+    <link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" />
+
+    <script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
+
+  
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .float-right {
+        float: right;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="{!! url('assets/css/app.css') !!}" rel="stylesheet">
+</head>
+<body>
+    
+    @include('layouts.partials.navbar')
+
+    <main class="container mt-5">
+        @yield('content')
+    </main>
+
+
+   
+    
+    @section("scripts")
+
+    @show
+    <script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/libs/metismenu/metisMenu.min.js')}}"></script>
+<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
+
+
+<!-- Required datatable js -->
+<script src="{{asset('assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<!-- Buttons examples -->
+<script src="{{asset('assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('assets/libs/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('assets/libs/pdfmake/build/pdfmake.min.js')}}"></script>
+<script src="{{asset('assets/libs/pdfmake/build/vfs_fonts.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-buttons/js/buttons.colVis.min.js')}}"></script>
+<!-- Responsive examples -->
+<script src="{{asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
+<!-- Datatable init js -->
+<script src="{{asset('assets/js/pages/datatables.init.js')}}"></script>
+<!-- Peity chart-->
+<script src="{{asset('assets/libs/peity/jquery.peity.min.js')}}"></script>
+<script src="{{asset('assets/js/app.js')}}"></script>
+
+
+<!-- Plugin Js-->
+<script src="{{asset('assets/libs/chartist/chartist.min.js')}}"></script>
+<script src="{{asset('assets/libs/chartist-plugin-tooltips/chartist-plugin-tooltip.min.js')}}"></script> 
+
+<script src="{{asset('assets/js/pages/dashboard.init.js')}}"></script>
+  </body>
+</html>
