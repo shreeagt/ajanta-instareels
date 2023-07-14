402634efd561a924dde41031adfc0a5a9624770e
RahulRawatAgt <rahulrawat.agt@gmail.com>
2023-07-14 16:57:43 +0530

    added the title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="description" content="">
-      <title>BIDINLS</title>
+      <title>Instareel</title>
       <meta name="author" content="">
       <meta name="generator" content="Hugo 0.87.0">
       <meta name="csrf-token" content="{{ csrf_token() }}">
